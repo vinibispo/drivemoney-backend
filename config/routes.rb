ccb898189cfe4195d5f9a6df0d87124128ebd09f
vinibispo <vini.bispo015@gmail.com>
2021-03-07 18:56:07 -0300

    feat(versioning): Versioning api using rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  namespace :api do
+    namespace :v1 do
+      if Rails.env.development?
+        mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+      end
+      post "/graphql", to: "graphql#execute"
+    end
   end
-  post "/graphql", to: "graphql#execute"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
