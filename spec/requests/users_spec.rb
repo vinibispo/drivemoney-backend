355057bf4e8c47d8b34280ca43c3941d41fc1255
vinibispo <vini.bispo015@gmail.com>
2021-03-11 22:48:25 -0300

    lint(users): Adding test to user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Users", type: :request do
   describe "POST /api/v1/login" do
-    let(:password) { FFaker::Internet.password(8)}
+    let(:password) { FFaker::Internet.password(8) }
     let(:user) { create(:user, password: password) }
     it "should login user" do
       post "/api/v1/login", params: {email: user[:email], password: password}
