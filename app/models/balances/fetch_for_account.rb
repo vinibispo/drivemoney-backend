eed1168746c2ecf0e9b31202b7ac93ed0bf109b9
vinibispo <vini.bispo015@gmail.com>
2021-03-18 22:37:15 -0300

    refactor(fetch_balance_for_account): Following second tip of @serradura use scope when you got enums

--- app/models/balances/fetch_for_account.rb
+++ app/models/balances/fetch_for_account.rb
@@ -5,20 +5,19 @@ module Balances
     def call!
       initial_value = account.initial_value
 
-      balance = account
-        .transactions
-        .where(status: :income)
-        .sum(:value) - account
-          .transactions
-          .where(status: :outcome)
-          .sum(:value) + initial_value
+      balance = (total_income - total_outcome) + initial_value
 
       Success result: {balance: balance}
     end
 
     private
 
-    def fetch_balance_for_account
+    def total_income
+      account.transactions.income.sum(:value)
+    end
+
+    def total_outcome
+      account.transactions.outcome.sum(:value)
     end
   end
 end
