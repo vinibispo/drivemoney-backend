ed1b44378a553390a734eebc30416399cf25989c
vinibispo <vini.bispo015@gmail.com>
2021-03-18 22:30:15 -0300

    refactor(fetch_balance_for_account): Following first tip of @serradura

--- app/models/balances/fetch_for_account.rb
+++ app/models/balances/fetch_for_account.rb
@@ -1,14 +1,10 @@
 module Balances
   class FetchForAccount < Micro::Case
     attribute :account
+
     def call!
-      fetch_balance_for_account
-    end
-
-    private
-
-    def fetch_balance_for_account
       initial_value = account.initial_value
+
       balance = account
         .transactions
         .where(status: :income)
@@ -16,7 +12,13 @@ module Balances
           .transactions
           .where(status: :outcome)
           .sum(:value) + initial_value
+
       Success result: {balance: balance}
+    end
+
+    private
+
+    def fetch_balance_for_account
     end
   end
 end
